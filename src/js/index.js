19c2ecc8a77fa84a1f88adc1aecda7d8e3b5dce7
Irina Semakova <semakovais@yandex.ru>
2025-06-28 17:20:38 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,80 @@
+import goblinImage from '../img/goblin.png';
+import '../css/style.css';
+
+class Game {
+  constructor() {
+    this.boardSize = 4;
+    this.currentPosition = null;
+    this.goblin = null;
+    this.interval = null;
+    this.initializeGame();
+  }
+
+  initializeGame() {
+    this.createBoard();
+    this.createGoblin();
+    this.startGame();
+  }
+
+  createBoard() {
+    const gameBoard = document.getElementById('gameBoard');
+    
+    // Clear existing board
+    while (gameBoard.firstChild) {
+      gameBoard.firstChild.remove();
+    }
+
+    for (let i = 0; i < this.boardSize; i++) {
+      const row = document.createElement('div');
+      row.className = 'row';
+      
+      for (let j = 0; j < this.boardSize; j++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.dataset.position = `${i}-${j}`;
+        row.append(cell);
+      }
+      
+      gameBoard.append(row);
+    }
+  }
+
+  createGoblin() {
+    this.goblin = document.createElement('img');
+    this.goblin.src = goblinImage;
+    this.goblin.className = 'goblin';
+    this.placeGoblinRandomly();
+  }
+
+  placeGoblinRandomly() {
+    // Remove from current position if exists
+    if (this.currentPosition) {
+      const currentCell = document.querySelector(`[data-position="${this.currentPosition}"]`);
+      if (currentCell && currentCell.contains(this.goblin)) {
+        this.goblin.remove();
+      }
+    }
+
+    // Get new random position
+    let newPosition;
+    do {
+      const row = Math.floor(Math.random() * this.boardSize);
+      const col = Math.floor(Math.random() * this.boardSize);
+      newPosition = `${row}-${col}`;
+    } while (newPosition === this.currentPosition);
+
+    this.currentPosition = newPosition;
+    const newCell = document.querySelector(`[data-position="${this.currentPosition}"]`);
+    newCell.append(this.goblin);
+  }
+
+  startGame() {
+    this.interval = setInterval(() => {
+      this.placeGoblinRandomly();
+    }, 1000);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Game();
+});
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[hash][ext][query]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
